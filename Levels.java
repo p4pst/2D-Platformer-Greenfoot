2f56365438094768e2466c7121ef024151ce7eea
wp23 <walter.pichl@hotmail.com>
2020-05-19 08:45:03 +0200

    Design

--- Levels.java
+++ Levels.java
@@ -3,30 +3,35 @@ import greenfoot.*;
 public class Levels extends World
 {
     GifImage bg = new GifImage("menu/bg.gif");
-    
+
     public Levels()
     {    
         super(1032, 624, 1);
         setBackground(bg.getCurrentImage());
         buildWorld();
     }
+
     public void buildWorld()
     {
         addObject(new GoBackToMenu(), 60, 605);
     }
+
     public void act()
     {
         setBackground(bg.getCurrentImage());
     }
+
     public void started()
     {
         Menu.sound.play();
         Menu.sound.setVolume(40);
     }
+
     public void stopped()
     {     
         Menu.sound.stop();
     }
+
     public void gameOver()
     {
         Menu.sound.stop();
