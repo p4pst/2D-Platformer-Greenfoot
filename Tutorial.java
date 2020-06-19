4a2a0e5fdb604a352e47571c6cc70864d46f111e
Drahtpfanne <sebastian.muric@hotmail.com>
2020-06-19 10:30:05 +0200

    a

--- Tutorial.java
+++ Tutorial.java
@@ -24,5 +24,25 @@ public class Tutorial extends World
         normalGrassBlock2.setLocation(295,428);
         Character character = new Character();
         addObject(character,150,356);
+        NormalGrassBlock normalGrassBlock4 = new NormalGrassBlock();
+        addObject(normalGrassBlock4,396,428);
+        normalGrassBlock4.setLocation(381,426);
+        NormalGrassBlock normalGrassBlock5 = new NormalGrassBlock();
+        addObject(normalGrassBlock5,478,424);
+        normalGrassBlock5.setLocation(472,424);
+        NormalGrassBlock normalGrassBlock6 = new NormalGrassBlock();
+        addObject(normalGrassBlock6,594,415);
+        normalGrassBlock6.setLocation(556,420);
+        normalGrassBlock6.setLocation(558,426);
+        GoBackToMenu goBackToMenu = new GoBackToMenu();
+        addObject(goBackToMenu,868,581);
+        goBackToMenu.setLocation(942,583);
     }
+    
+    public void act()
+    {
+        getBackground().drawImage(new GreenfootImage("Press A or D to move", 30, null, null), 520, 520);
+        getBackground().drawImage(new GreenfootImage("Press space to jump", 30, null, null), 540, 80);
+        getBackground().drawImage(new GreenfootImage("Press left mouse button to shoot", 30, null, null), 80, 70);
+    } 
 }
